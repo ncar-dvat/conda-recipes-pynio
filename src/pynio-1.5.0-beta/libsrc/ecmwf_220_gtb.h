a74ef24605c5ecf7ead1900cb0c5102c844b5bdf
Dave Brown <dbrown@ucar.edu>
2016-01-04 02:52:04 -0700

    now using git to get pynio source

--- src/pynio-1.5.0-beta/libsrc/ecmwf_220_gtb.h
+++
@@ -1,9 +0,0 @@
-/*
- * Center: 98 
- * Subcenter: 
- * Parameter table version: 220
- */
-
-TBLE2 ecmwf_220_params[] = {
-{228, "Total precipitation observation count", "dimensionless", "TPOC"}
-};
--- src/pynio-1.5.0-beta/libsrc/nioUtils.h
+++
@@ -1,5 +0,0 @@
-#ifndef _nioUtils_h_
-#define _nioUtils_h_
-extern int _MachineIsBigEndian();
-#endif
-
